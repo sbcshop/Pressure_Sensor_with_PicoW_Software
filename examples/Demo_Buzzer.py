6a8d7ef95f6625db184349ce67fa27d6a9316b32
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2023-08-10 11:48:14 +0530

    Create Demo_Buzzer.py

---
+++ examples/Demo_Buzzer.py
@@ -0,0 +1,18 @@
+# Demo code to test buzzer with Pico W
+from machine import Pin, PWM
+import time
+
+buzzer = PWM(Pin(15)) #define PWM output pin GP15
+
+def playtone(frequency): # function to play tone on buzzer with define frequency
+    buzzer.duty_u16(5000)
+    buzzer.freq(frequency)
+
+def bequiet():	# Function to stop buzzer
+    buzzer.duty_u16(0)
+
+while True:
+    playtone(1865)
+    time.sleep(0.5)
+    bequiet()
+    time.sleep(0.5)
